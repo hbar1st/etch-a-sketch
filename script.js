98924edbca2d52b2d0fe0e9fcaf1de724cf8be34
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-09-30 21:39:07 -0400

    allow user to select grid size

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 const mainEl = document.querySelector("main");
+const button = document.querySelector("button");
+const span = document.querySelector("span");
+
+let width = 16;
 
 mainEl.addEventListener("mouseover", draw);
+button.addEventListener("click", () => {
+    let w = 0;
+    do {
+        w = prompt("Choose grid width [1-100]:");
+    } while (!w | isNaN(w) || w < 1 || w > 100);
+    renderGrid(w);
+})
 
-for (let i = 0; i < 16; i++) {
-    const outerDivEl = document.createElement("div");
-    mainEl.appendChild(outerDivEl);
-    for (let j = 0; j < 16; j++ ) {
-        const innerDivEl = document.createElement("div");
-        innerDivEl.setAttribute("class", "square");
-        outerDivEl.appendChild(innerDivEl);
+function renderGrid(width) {
+    mainEl.innerHTML="";
+    for (let i = 0; i < width; i++) {
+        const outerDivEl = document.createElement("div");
+        mainEl.appendChild(outerDivEl);
+        for (let j = 0; j < width; j++) {
+            const innerDivEl = document.createElement("div");
+            innerDivEl.setAttribute("class", "square");
+            outerDivEl.appendChild(innerDivEl);
+        }
     }
+    span.innerText = `${width}x${width}`;
 }
 
-function draw (e) {
-    if ( e.target.classList.contains("square") ) {
-        console.log("This is a square");
+function draw(e) {
+    if (e.target.classList.contains("square")) {
         e.target.style.backgroundColor = "black";
     }
 }
+
+renderGrid(width);
