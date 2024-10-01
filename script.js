6b1c6de0a19ac6659905d40217a1b2664fe05a56
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-10-01 12:30:48 -0400

    Add feature to darken cells the more times they are moused over

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const mainEl = document.querySelector("main");
-const button = document.querySelector("button");
-const span = document.querySelector("span");
+const selectSizeBtn = document.querySelector("button");
+const sizeSpan = document.querySelector("span");
 
-let width = 16;
+let width = 16; //initial width of grid
 
 mainEl.addEventListener("mouseover", draw);
-button.addEventListener("click", () => {
+
+selectSizeBtn.addEventListener("click", () => {
     let w = 0;
     do {
         w = prompt("Choose grid width [1-100]:");
@@ -13,8 +14,12 @@ button.addEventListener("click", () => {
     renderGrid(w);
 })
 
+/** 
+ * renderGrid: the "main" function that runs first
+ * created the grid divs and adds them to the DOM
+ */
 function renderGrid(width) {
-    mainEl.innerHTML="";
+    mainEl.innerHTML = ""; //clear the old grid first
     for (let i = 0; i < width; i++) {
         const outerDivEl = document.createElement("div");
         mainEl.appendChild(outerDivEl);
@@ -24,17 +29,48 @@ function renderGrid(width) {
             outerDivEl.appendChild(innerDivEl);
         }
     }
-    span.innerText = `${width}x${width}`;
+    sizeSpan.innerText = `${width}x${width}`;
 }
 
+/**
+ * colors the cells randomly or darkens the cell if revisited
+ * @param {*} e 
+ */
 function draw(e) {
     if (e.target.classList.contains("square")) {
-        //e.target.style.backgroundColor = "black";
         let red = getRandomValue();
         let green = getRandomValue();
         let blue = getRandomValue();
-        e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+        let bgColor = `rgb(${red}, ${green}, ${blue})`;
+        let style = e.target.style.backgroundColor;
+        const regex = /rgb\((\d+), (\d+), (\d+)\)/;
+        let match = style.match(regex) ; 
+        if (style && match[1]) {
+            bgColor = darken(+match[1], +match[2], +match[3]);
+        }
+        
+        e.target.style.backgroundColor = bgColor;
     }
+}
+
+/**
+ * Returns an rgb string with 10% darker colors
+ * 
+ * @param {*} r 
+ * @param {*} g 
+ * @param {*} b 
+ * @returns 
+ */
+function darken(r,g,b) {
+    let red = r;
+    let green = g;
+    let blue = b;
+
+    red = r < 25 ? 0 : r - 25;
+    green = g < 25 ? 0 : g - 25;
+    blue = b < 25 ? 0 : b - 25;
+
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
 function getRandomValue() {
